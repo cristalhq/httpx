a415217848fcce82dcadea72a3ceca1506ad5d78
Oleg Kovalov <oleg@hey.com>
2022-09-08 17:03:51 +0200

    Add ErrorResponse

---
+++ httpx.go
@@ -0,0 +1,47 @@
+package httpx
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// ErrorResponse return and error wrapped into JSON.
+func ErrorResponse(w http.ResponseWriter, code int, err error) {
+	if err == nil {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.WriteHeader(code)
+		return
+	}
+
+	msg := err.Error()
+
+	data := struct {
+		Code  int    `json:"code"`
+		Error string `json:"error"`
+	}{
+		Code:  code,
+		Error: msg,
+	}
+
+	raw, errMarsh := json.Marshal(data)
+	if errMarsh != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("JSON marshal failed: %v", errMarsh)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(code)
+	_, _ = w.Write(prettyJSON(raw))
+}
+
+// TODO: enable by compile flag
+func prettyJSON(b []byte) []byte {
+	var out bytes.Buffer
+	_ = json.Indent(&out, b, "", "  ")
+	return out.Bytes()
+}
