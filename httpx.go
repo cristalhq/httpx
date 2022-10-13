8542c8dbf5228152bbe950be36eb11b94058ffbc
Oleg Kovalov <oleg@hey.com>
2022-10-13 18:47:50 +0200

    Better Error type and response

--- httpx.go
+++ httpx.go
@@ -1,45 +1,42 @@
 package httpx
 
 import (
-	"bytes"
 	"encoding/json"
-	"log"
 	"net/http"
 )
 
 // Error for a request.
 type Error struct {
-	Code  int    `json:"code,omitempty"`
-	Error string `json:"error,omitempty"`
-	Type  string `json:"type,omitempty"`
+	Code    int    `json:"code,omitempty"`
+	Type    string `json:"type,omitempty"`
+	Message string `json:"msg,omitempty"`
+}
+
+func (e Error) Error() string {
+	return e.Message
 }
 
 // ErrorResponse return and error wrapped into JSON.
 func ErrorResponse(w http.ResponseWriter, code int, err error) {
-	data := Error{
-		Code: code,
-		// TODO: add Type
-	}
-
-	if err != nil {
-		data.Error = err.Error()
-	}
-
-	raw, errMarsh := json.Marshal(data)
-	if errMarsh != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf("JSON marshal failed: %v", errMarsh)
+	if err == nil {
+		w.WriteHeader(code)
 		return
 	}
 
+	var resp *Error
+	if e, ok := err.(*Error); ok {
+		resp = e
+	} else {
+		resp = &Error{
+			Message: err.Error(),
+		}
+	}
+	resp.Code = code
+
+	// TODO: indent under compile flag?
+	raw, _ := json.MarshalIndent(resp, "", "  ")
+
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
-	_, _ = w.Write(prettyJSON(raw))
-}
-
-// TODO: enable by compile flag?
-func prettyJSON(b []byte) []byte {
-	var out bytes.Buffer
-	_ = json.Indent(&out, b, "", "  ")
-	return out.Bytes()
+	w.Write(raw)
 }
