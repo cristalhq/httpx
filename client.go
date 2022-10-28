bc6da4626a76762d4bcf70679bfac82781a84274
Oleg Kovalov <oleg@hey.com>
2022-10-28 19:41:43 +0200

    Add NewClient

--- client.go
+++ client.go
@@ -1,8 +1,18 @@
 package httpx
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 // Doer is an interface for http.Client.
 type Doer interface {
 	Do(req *http.Request) (*http.Response, error)
+}
+
+// NewClient return http.Client with a sane default.
+func NewClient() *http.Client {
+	return &http.Client{
+		Timeout: 5 * time.Second,
+	}
 }
